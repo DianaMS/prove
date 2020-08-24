eb896cc963d05a0bd2980744141b00b87f2c2960
DianaMS <dmartinezs@uni.pe>
2020-08-24 02:01:28 -0500

    añade className a elementos

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -9,12 +9,12 @@ const Item = ({ img, title, description, buttonleft, buttonright }) => (
       </section>
       <section className='description-content'>
         <div className='general-description'>
-          <p>{title}</p>
-          <p>{description}</p>
+          <p className='title'>{title}</p>
+          <p className='description'>{description}</p>
         </div>
         <div className='routes'>
-          <button type='button'>{buttonleft}</button>
-          <button type='button'>{buttonright}</button>
+          <button type='button' className='button-left'>{buttonleft}</button>
+          <button type='button' className='button-right'>{buttonright}</button>
         </div>
       </section>
     </div>
