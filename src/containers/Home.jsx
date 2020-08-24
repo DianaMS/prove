b3c96cf82f9fa9c0b79a813f3eef89a6a0eb0157
DianaMS <dmartinezs@uni.pe>
2020-08-24 02:02:27 -0500

    reestructura del home

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -19,11 +19,10 @@ const Home = () => (
           <h2 className='hello'>Hola Soy Diana</h2>
           <h3 className='slogan'>mi Slogan</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <p>¡ Hacerlo con pasión o nada !</p>
         </div>
       </section>
-      <section className='gallery'>
-        <p>¡ Hacerlo con pasión o nada !</p>
-      </section>
+      <section className='gallery' />
     </section>
 
   </>
