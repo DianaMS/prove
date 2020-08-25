92793ec02e3bf269d446d7efca6e9fe95fd21739
DianaMS <dmartinezs@uni.pe>
2020-08-24 19:38:59 -0500

    síntesis de data con map

--- src/containers/Projects.jsx
+++ src/containers/Projects.jsx
@@ -1,61 +1,23 @@
 import React from 'react';
 import Item from '../components/Item';
 import '../assets/styles/components/Projects.scss';
-
-const giff = {
-  uno: 'src/assets/static/cardValidation.gif',
-  dos: 'src/assets/static/cipher.gif',
-  tres: 'src/assets/static/pokemonGo.gif',
-  cuatro: 'src/assets/static/mdLinks.gif',
-  cinco: 'src/assets/static/redSocial.gif',
-  seis: 'src/assets/static/burgerApi.gif',
-};
+import data from '../data/data';
 
 const Projects = () => (
 
   <section className='container-projects'>
-    <Item
-      img={giff.uno}
-      title='POKEMON GO'
-      description='Aqui va toda la descripción'
-      buttonleft='README'
-      buttonright='DESPLIEGUE'
-    />
-    <Item
-      img={giff.dos}
-      title='POKEMON GO'
-      description='Aqui va toda la descripción'
-      buttonleft='README'
-      buttonright='DESPLIEGUE'
-    />
-    <Item
-      img={giff.tres}
-      title='POKEMON GO'
-      description='Aqui va toda la descripción'
-      buttonleft='README'
-      buttonright='DESPLIEGUE'
-    />
-    <Item
-      img={giff.cuatro}
-      title='POKEMON GO'
-      description='Aqui va toda la descripción'
-      buttonleft='README'
-      buttonright='DESPLIEGUE'
-    />
-    <Item
-      img={giff.cinco}
-      title='POKEMON GO'
-      description='Aqui va toda la descripción'
-      buttonleft='README'
-      buttonright='DESPLIEGUE'
-    />
-    <Item
-      img={giff.seis}
-      title='POKEMON GO'
-      description='Aqui va toda la descripción'
-      buttonleft='README'
-      buttonright='DESPLIEGUE'
-    />
+    {data.projects.map((e, index) => {
+      return (
+        <Item
+          key={index}
+          img={e.gif}
+          title={e.title}
+          description={e.description}
+          buttonleft={e.buttonleft}
+          buttonright={e.buttonright}
+        />
+      );
+    })}
   </section>
 
 );
